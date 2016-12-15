e93e9af57b36f62cf8ee32b2b22828047eff2c7e
Vitaly Domnikov <dotcypress@gmail.com>
2016-12-15 09:30:00 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# hyper-material
+
+Material theme for [Hyper.app](https://hyper.is).
+
+<img width="640" alt="hyper-material" src="https://cloud.githubusercontent.com/assets/1333916/21167023/63bc12ac-c15e-11e6-8694-c3a1b5c4afa6.png">
+
+## Install
+
+Add `hyper-material` to your plugins list in `~/.hyperterm.js`:
+
+```js
+plugins: [
+  // a list of plugins to fetch and install from npm
+  // format: [@org/]project[#version]
+  // examples:
+  //   `hyperpower`
+  //   `@company/project`
+  //   `project#1.0.1`
+  plugins: [
+    'hyper-material'
+  ]
+]
+```
---
+++ index.js
@@ -0,0 +1,66 @@
+exports.onWindow = browserWindow => browserWindow.setVibrancy('ultra-dark');
+exports.decorateConfig = (config) => {
+  return Object.assign({}, config, {
+    foregroundColor: '#eceff1',
+    backgroundColor: 'rgba(38, 50, 56, 0.9)',
+    borderColor: 'transparent',
+    cursorColor: '#68b6f3',
+    colors: [
+      '#263238',
+      '#ff5252',
+      '#9ccc65',
+      '#fee94e',
+      '#2b98f0',
+      '#b38bfc',
+      '#68b6f3',
+      '#eceff1',
+      '#617d8a',
+      '#fc625d',
+      '#9ccc65',
+      '#fee94e',
+      '#2b98f0',
+      '#b38bfc',
+      '#68b6f3',
+      '#ffffff'
+    ],
+    termCSS: `
+      ${config.termCSS || ''}
+      @keyframes blink-animation {
+        to {
+          background-color: transparent;
+        }
+      }
+      .cursor-node[focus=true]:not([moving]) {
+        animation: blink-animation .777s ease-in-out infinite;
+        box-sizing: content-box !important;
+        mix-blend-mode: difference;
+      }
+    `,
+    css: `
+      ${config.css || ''}
+      .header_header {
+        background: rgba(34, 45, 50, 0.5) !important;
+      }
+      .hyperterm_main, .tab_tab, .tab_text {
+        border: none !important;
+        padding: 0 !important;
+      }
+      .tab {
+        color: red !important;
+      }
+      .tab_tab::before {
+        position: absolute;
+        content: "";
+        display: block;
+        left: 0px;
+        right: 0px;
+        bottom: 0px;
+        transition: border-color .3s ease-in-out;
+        border-bottom: 3px solid transparent;
+      }
+      .tab_active::before {
+        border-bottom: 3px solid #a04a92;
+      }
+    `
+  })
+}
